89d1064ad27961e9de3e00b575854f445d155818
Cillian <cillsm80@gmail.com>
2023-10-16 13:13:32 +0100

    part2b done

--- Events/src/Lab3Part1b.java
+++ Events/src/Lab3Part1b.java
@@ -1,2 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Lab3Part1b {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        // config frame
+        frame.setLocation(200, 200);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Translate");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints(); // create constraints variable for gridbag layout
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(25,25,25,25);
+
+        JButton button;
+
+        button = new JButton("My text will change when pushed");
+        c.gridx = 1;
+        c.gridy = 2;
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button.setText("See? I told you!");
+            }
+        });
+        panel.add(button, c);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
