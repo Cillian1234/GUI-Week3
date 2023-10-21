5140db2952a02efa9b1aac512ad8ebcde10b455f
Cillian <126096645+CillianSmee@users.noreply.github.com>
2023-10-21 22:05:10 +0100

    part 3 done

--- Events/src/Lab3Part3.java
+++ Events/src/Lab3Part3.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,22 +26,46 @@ public class Lab3Part3 {
         c.fill = GridBagConstraints.HORIZONTAL;
         c.insets = new Insets(25,25,25,25);
 
-        String[] coloursArray = {"Red", "Green", "Blue"};
+        String[] colourNames = {"Red", "Green", "Blue"};
+        Color[] coloursArray = {Color.RED, Color.GREEN, Color.BLUE};
+        String[] imageNames = {"Mike", "Cat", "Dog"};
         ImageIcon[] imagesArray = getImages();
 
-        JComboBox colours = new JComboBox(coloursArray);
+        JComboBox colours = new JComboBox(colourNames);
         c.gridx = 0;
         c.gridy = 0;
         panel.add(colours, c);
 
-        JComboBox images = new JComboBox(imagesArray);
+        JComboBox images = new JComboBox(imageNames);
         c.gridx = 1;
         c.gridy = 0;
         panel.add(images, c);
 
-        JLabel imgLabel = new JLabel("Label");
+        JLabel imgLabel = new JLabel("An image will appear here");
         c.gridx = 1;
+        c.gridy = 1;
+        panel.add(imgLabel, c);
 
+        JLabel desc = new JLabel("Description will appear here", SwingConstants.CENTER);
+        c.gridx = 0;
+        c.gridy = 1;
+        desc.setOpaque(true);
+        panel.add(desc, c);
+
+        images.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                imgLabel.setIcon(imagesArray[images.getSelectedIndex()]);
+                imgLabel.setText(null);
+            }
+        });
+
+        colours.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                desc.setBackground(coloursArray[colours.getSelectedIndex()]);
+            }
+        });
 
         frame.add(panel);
         frame.setVisible(true);
